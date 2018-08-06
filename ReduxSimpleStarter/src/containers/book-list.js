f7501116b5c99f94bfba918d14eec16872b21158
kirilzh <kiril.zhelyazkov9@gmail.com>
2018-08-06 21:31:27 +0300

    added active book reducer

--- ReduxSimpleStarter/src/containers/book-list.js
+++ ReduxSimpleStarter/src/containers/book-list.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class BookList extends Component {
+import { selectBook } from '../actions/index';
+
+class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
       return (
-        <li key={book.title} classname="list-group-item">
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item"
+        >
           {book.title}
         </li>
       );
@@ -14,3 +22,24 @@ export default class BookList extends Component {
     return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+// anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, the result should be
+  // passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+// promote BookList from a Component to a container
+// it needs to know about the mapDispatchToProps
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BookList);
---
+++ ReduxSimpleStarter/src/reducers/reducer_active_book.js
@@ -0,0 +1,10 @@
+// state argument is not application state, only the state
+// this reducer is responsible for
+export default function(state = null, action) {
+  // if undefined state = null
+  switch (action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+  return state;
+}
